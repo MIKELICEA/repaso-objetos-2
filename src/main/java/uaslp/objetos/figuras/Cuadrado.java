07c76130ee263a32b2ce7e6c652572a7f3867979
MIKELICEA <miguel_ll1206@hotmail.com>
2022-11-28 19:23:27 -0600

    Ejercicio 1

--- src/main/java/uaslp/objetos/figuras/Cuadrado.java
+++ src/main/java/uaslp/objetos/figuras/Cuadrado.java
@@ -1,4 +1,34 @@
 package uaslp.objetos.figuras;
 
 public class Cuadrado {
+    private double lado;
+    private double area;
+
+    public Cuadrado(double lado)
+    {
+        this.lado=lado;
+    }
+
+    public Cuadrado()
+    {
+
+    }
+
+    public void setLado(double lado)
+    {
+        this.lado=lado;
+    }
+
+    public double getArea()
+    {
+        area = lado * lado;
+
+        return area;
+    }
+
+    public double getLado()
+    {
+        return lado;
+    }
 }
+
---
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -0,0 +1,34 @@
+package uaslp.objetos.figuras;
+
+public class PoligonoRegular {
+
+    private double lado;
+    private int numeroDeLados;
+    private double area;
+
+    public PoligonoRegular(int numeroDeLados) {
+        this.numeroDeLados = numeroDeLados;
+    }
+
+    public PoligonoRegular(int numeroDeLados, double lado) {
+        this.numeroDeLados = numeroDeLados;
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea() {
+        double perimetro, apotema, anguloCentral;
+        perimetro = numeroDeLados * lado;
+        anguloCentral = 360 / (double) numeroDeLados;
+        apotema = lado / (2 * Math.tan(Math.toRadians(anguloCentral / 2)));
+        area = (perimetro * apotema) / 2;
+        return area;
+    }
+}
